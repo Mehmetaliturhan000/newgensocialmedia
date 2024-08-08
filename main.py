81cfdfefe6d269330ef56034340e87a65f9d24b3
mehmetaliturhan000 <memeydos2000@gmail.com>
2024-08-08 15:11:37 +1000

    post method added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -11,5 +12,6 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts():
-    return {"message": "succesfully created posts"}
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']} "}
