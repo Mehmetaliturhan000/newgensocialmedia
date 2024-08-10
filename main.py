8cb0d89eef8002e3f3cbd776bbb57afe3da627eb
mehmetaliturhan000 <memeydos2000@gmail.com>
2024-08-10 16:44:35 +1000

    base model validation added

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def root():
@@ -12,6 +21,8 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']} content: {payload['content']} "}
+def create_posts(post: Post):
+    print(post)
+    print(post.dict())
+    return {"data" : post}
+ #title str, content str   
