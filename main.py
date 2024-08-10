dc4218be8971ba41006573fcd1d76a02c4a10b6b
mehmetaliturhan000 <memeydos2000@gmail.com>
2024-08-11 01:41:25 +1000

    my_posts list added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -12,17 +13,21 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title" : "title of post 1", "content" : "content of post 1", "id": 1} , {"title" : "favorite foods", "content": "I like pizza", "id" : 2}]    
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API!!!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
-    print(post)
-    print(post.dict())
-    return {"data" : post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,10000000)
+    my_posts.append(post_dict)
+
+    return {"data" : post_dict}
  #title str, content str   
