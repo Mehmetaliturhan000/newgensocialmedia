b3c77add8c5a895ce4eefa14b3e546930599b7fe
mehmetaliturhan000 <memeydos2000@gmail.com>
2024-08-11 14:03:48 +1000

    get post by id added

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ class Post(BaseModel):
 
 my_posts = [{"title" : "title of post 1", "content" : "content of post 1", "id": 1} , {"title" : "favorite foods", "content": "I like pizza", "id" : 2}]    
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p  
+
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API!!!"}
@@ -31,3 +37,8 @@ def create_posts(post: Post):
 
     return {"data" : post_dict}
  #title str, content str   
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post detail" : post}
